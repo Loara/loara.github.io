fbd991e5e4b6ce6e641420479383598b91d88198
Loara <loara@LoaraPC>
2018-10-29 13:16:43 +0100

    tutoraggio

---
+++ GLib.js
@@ -0,0 +1,98 @@
+var mese = ["gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", 
+	"luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"];
+
+var gsett = ["domenica", "lunedì", "martedì", "mercoledì", "giovedì", "venerdì", "sabato"];
+
+	/*
+	n = intervallo in cui non si va
+	i = giorno in cui si va
+	Vanno messi in ORDINE CRESCENTE
+	*/
+var impegni = ["n", "11/1/2018", "11/4/2018", "n", "12/24/2018", "1/21/2019"];
+
+function geti(i){
+	var ret = new Date(impegni[i]);
+	ret.setHours(16);
+	return ret;
+}
+
+function printDate(d){
+	return gsett[d.getDay()] + " " + d.getDate() + " " + mese[d.getMonth()] + " " + d.getFullYear();
+}
+
+function prossimoIncontro(d){
+	/*
+	0 è domenica, quindi giovedì è 4 modulo 7
+	In aritmetica modulo 7 se i è il giorno corrente (da 0 a 6) ed n esprime quanti giorni mancano al prossimo giovedì allora i + n = 4 e quindi
+	n = 4 - i = 4 + 7 - i = 11 - i
+	*/
+	var i = d.getDay();
+	var n = (11 - i) % 7;
+	if(i == 4 && d.getHours() > 16){
+		n = 7;
+	}
+	var ret = new Date(d.toString());
+	ret.setDate(d.getDate() + n);
+	ret.setHours(16);
+	return ret;
+}
+
+function SprossimoIncontro(d){
+	/*
+	parte sempre il giorno dopo
+	*/
+	var i = d.getDay();
+	var n = (11 - i) % 7;
+	if(n == 0){
+		n = 7;
+	}
+	var ret = new Date(d.toString());
+	ret.setDate(d.getDate() + n);
+	ret.setHours(16);
+	return ret;
+}
+
+function primoBuono(d, c){
+	//c è il giorno corrente, vanno eliminati gli eventi passati
+	var i, j, len, ret;
+	len = impegni.length;
+	for(i=0; i<len; i++){
+		if(impegni[i] == "n"){
+			if(c <= geti(i+2))
+				break;
+			else
+				i += 2;
+		}
+		else{
+			if(c <= geti(i+1))
+				break;
+			else
+				i++;
+		}
+	}
+	if(i >= len)
+		return d;
+	for(j = i; j < len; j++){
+		if(impegni[j] == "n"){
+			if(d >= geti(j+1) && d <= geti(j+2)){
+				return primoBuono(SprossimoIncontro(geti(j+2)), c);
+			}
+			else
+				j += 2;
+		}
+		else if(impegni[j] == "i"){
+			if(d >= geti(j+1)){
+				return geti(j+1);
+			}
+			else
+				j += 1;
+		}
+	}
+	return d;
+}
+
+function ppi(c){
+	return primoBuono(prossimoIncontro(c), c);
+}
+
+
