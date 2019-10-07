2cc5ecaaa9e3b501b95b7998fe6fdfa1c55d2740
Loara <loara@LoaraPC>
2019-10-07 14:33:05 +0200

    altre attivita

--- assets/js/switch.js
+++ assets/js/switch.js
@@ -45,7 +45,7 @@ function respage2(){
 
 function mainpage(){
 	rmall();
-	document.getElementById("main").innerHTML = "<div><img src=\"assets/img/bishop.jpg\" style=\"max-width:25%;height:auto;margin-top:-50px; border-radius:10%\"></div><div><h2 style=\"font-family: \'Indie Flower\';margin: 0 5%\">\"Io penso che all'origine della creatività ci sia quella che io chiamo la capacità o la disponibilità a sognare; a immaginare mondi diversi, cose diverse, cercando di combinarle nella propria immaginazione in vario modo.\"</h2><h3 style=\"margin: 0 10%;text-align:right;font-family: \'Indie Flower\';font-style:italic\">Ennio De Giorgi</h3>";
+	document.getElementById("main").innerHTML = "<div><img src=\"assets/img/bishop.jpg\" class=\"maimage\"></div><div><h2 style=\"font-family: \'Indie Flower\';margin: 0 5%\">\"Io penso che all'origine della creatività ci sia quella che io chiamo la capacità o la disponibilità a sognare; a immaginare mondi diversi, cose diverse, cercando di combinarle nella propria immaginazione in vario modo.\"</h2><h3 style=\"margin: 0 10%;text-align:right;font-family: \'Indie Flower\';font-style:italic\">Ennio De Giorgi</h3>";
 	document.getElementById("hom").classList.add("active");
 	document.getElementById("mtitle").innerHTML = "Paolo De Donato";
 }
@@ -61,18 +61,20 @@ function curpage(){
 	rmall();
 	document.getElementById("main").innerHTML = "<div class=\"cur_cont\"><div class=\"cur_ltr\"></div><div class=\"cur_ctr\">\
 		<h2>Formazione</h2>\
-		<div class=\"cur_row\"><div class=\"cur_date\"><p>2014</p></div><div class=\"cur_des\"><p>Maturità scientifica</p></div></div>\
-		<div class=\"cur_row\"><div class=\"cur_date\"><p>2017</p></div><div class=\"cur_des\"><p>Laurea in Matematica all\' Università degli Studi di Napoli \"Federico II\"</p><p>Titolo tesi: Il teorema di Rouché e applicazioni</p><p>Relatore: Luigi Greco</p><p>Voto di laurea: 110/110 <i>cum laude</i></p></div></div>\
 		<div class=\"cur_row\"><div class=\"cur_date\"><p>2019</p></div><div class=\"cur_des\"><progress>Coming soon...</progress></div></div>\
+		<div class=\"cur_row\"><div class=\"cur_date\"><p>2017</p></div><div class=\"cur_des\"><p>Laurea in Matematica all\' Università degli Studi di Napoli \"Federico II\"</p><p>Titolo tesi: Il teorema di Rouché e applicazioni</p><p>Relatore: Luigi Greco</p><p>Voto di laurea: 110/110 <i>cum laude</i></p></div></div>\
+		<div class=\"cur_row\"><div class=\"cur_date\"><p>2014</p></div><div class=\"cur_des\"><p>Maturità scientifica</p></div></div>\
 		<h2>Riconoscimenti e premi</h2>\
-		<div class=\"cur_row\"><div class=\"cur_date\"><p>5-8 maggio 2011</p></div><div class=\"cur_des\"><p>Ammissione fase nazionale delle Olimpiadi nazionali della Matematica a Cesenatico.</p></div></div>\
-		<div class=\"cur_row\"><div class=\"cur_date\"><p>9-12 maggio 2013</p></div><div class=\"cur_des\"><p>Medaglia di bronzo alle Olimpiadi nazionali della Matematica a Cesenatico.</p></div></div>\
 		<div class=\"cur_row\"><div class=\"cur_date\"><p>8-11 maggio 2014</p></div><div class=\"cur_des\"><p>Medaglia d'argento alle Olimpiadi nazionali della Matematica a Cesenatico.</p></div></div>\
+		<div class=\"cur_row\"><div class=\"cur_date\"><p>9-12 maggio 2013</p></div><div class=\"cur_des\"><p>Medaglia di bronzo alle Olimpiadi nazionali della Matematica a Cesenatico.</p></div></div>\
+		<div class=\"cur_row\"><div class=\"cur_date\"><p>5-8 maggio 2011</p></div><div class=\"cur_des\"><p>Ammissione fase nazionale delle Olimpiadi nazionali della Matematica a Cesenatico.</p></div></div>\
 		<h2>Seminari e conferenze</h2>\
-		<div class=\"cur_row\"><div class=\"cur_date\"><p>16-19 dicembre 2018</p></div><div class=\"cur_des\"><p>Partecipazione al ciclo di seminari \"Junior Math Days\" presso il dipartimento di matematica della SISSA a Trieste.</p></div></div>\
-		<div class=\"cur_row\"><div class=\"cur_date\"><p>16-17 aprile 2019</p></div><div class=\"cur_des\"><p>Partecipazione al ciclo di seminari \"Some topics of Geometric Analysis and Geometric Measure Theory\" presso il centro di ricerca matematica Ennio De Giorgi a Pisa.</p></div></div>\
 		<div class=\"cur_row\"><div class=\"cur_date\"><p>22 luglio -16 agosto 2019</p></div><div class=\"cur_des\"><p>Partecipazione alla Scuola Matematica Interuniversitaria di Perugia, durante il quale sono stati seguiti i seguenti corsi:</p> <p><b>Analisi Funzionale</b> (tenuto dal prof. Marius Ghergu, votazione A+)</p> <p><b>Geometria Differenziale</b> (tenuto dal prof. Liviu Ornea, votazione A++).</p></div></div>\
-		</div><div class=\"cur_ltr\"></div></div>";
+		<div class=\"cur_row\"><div class=\"cur_date\"><p>16-17 aprile 2019</p></div><div class=\"cur_des\"><p>Partecipazione al ciclo di seminari \"Some topics of Geometric Analysis and Geometric Measure Theory\" presso il centro di ricerca matematica Ennio De Giorgi a Pisa.</p></div></div>\
+		<div class=\"cur_row\"><div class=\"cur_date\"><p>16-19 dicembre 2018</p></div><div class=\"cur_des\"><p>Partecipazione al ciclo di seminari \"Junior Math Days\" presso il dipartimento di matematica della SISSA a Trieste.</p></div></div>\
+		<h2>Altre attività</h2>\
+		<div class=\"cur_row\"><div class=\"cur_date\">1 novembre 2018 - 6 settembre 2019</div><div class=\"cur_des\">Tutorato Analisi 1 per i ragazzi del primo anno del corso di laurea in Matematica presso l'Università degli Studi di Napoli \"Federico II\"</div></div></div>\
+		<div class=\"cur_ltr\"></div></div>";
 	document.getElementById("cur").classList.add("active");
 	document.getElementById("mtitle").innerHTML = "Curriculum";
 }
